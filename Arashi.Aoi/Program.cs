035b97a4dbad31a0bce6851806a149c1ccbf38b8
Milkey Tan <819774989@qq.com>
2020-06-24 21:39:58 +0800

    args

--- Arashi.Aoi/Program.cs
+++ Arashi.Aoi/Program.cs
@@ -16,8 +16,13 @@ namespace Arashi.Aoi
                 {Name = "Arashi.Aoi", Description = "ArashiDNS.Aoi - Simple Lightweight DNS over HTTPS Server"};
 
             cmd.HelpOption("-?|-h|--help");
-            var ipOption = cmd.Option<string>("-l|--listen <IPEndPoint>", "Set listen address and port",
+            var ipOption = cmd.Option<string>("-l|--listen <IPEndPoint>", "Set listen ip address and port <127.0.0.1:2020>",
                 CommandOptionType.SingleValue);
+            var upOption = cmd.Option<string>("-up|--upstream <IPAddress>", "Set upstream ip address <8.8.8.8>",
+                CommandOptionType.SingleValue);
+            var cacheOption = cmd.Option("--cache", "Set enable caching", CommandOptionType.NoValue);
+            var chinaListOption = cmd.Option("--chinalist", "Set enable chinalist", CommandOptionType.NoValue);
+            var logOption = cmd.Option("--log", "Set enable log", CommandOptionType.NoValue);
 
             cmd.OnExecute(() =>
             {
@@ -25,6 +30,10 @@ namespace Arashi.Aoi
                 var ipEndPoint = ipOption.HasValue()
                     ? IPEndPoint.Parse(ipOption.Value())
                     : new IPEndPoint(IPAddress.Loopback, 2020);
+                if (upOption.HasValue()) Config.UpStream = IPAddress.Parse(upOption.Value());
+                Config.CacheEnable = cacheOption.HasValue();
+                Config.ChinaListEnable = chinaListOption.HasValue();
+                Config.LogEnable = logOption.HasValue();
 
                 var host = new WebHostBuilder()
                     .UseKestrel()
