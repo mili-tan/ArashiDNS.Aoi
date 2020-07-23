a32872d9de03a6210dbea690bf502f234a27f3ae
Milkey Tan <819774989@qq.com>
2020-07-23 19:39:18 +0800

    sth

--- Arashi.Azure/Program.cs
+++ Arashi.Azure/Program.cs
@@ -5,16 +5,10 @@ namespace Arashi.Azure
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(
+                webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
