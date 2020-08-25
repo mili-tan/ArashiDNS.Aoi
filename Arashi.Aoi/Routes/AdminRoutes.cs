82b674c96cf02b074e1590eebebf759cc29ddf03
Milkey Tan <819774989@qq.com>
2020-08-25 19:49:15 +0800

    AdminToken

--- Arashi.Aoi/Routes/AdminRoutes.cs
+++ Arashi.Aoi/Routes/AdminRoutes.cs
@@ -18,51 +18,30 @@ namespace Arashi.Aoi.Routes
         {
             endpoints.Map(Config.AdminPerfix + "/cache/ls", async context =>
             {
-                context.Response.Headers.Add("X-Powered-By", "ArashiDNSP/ONE.Aoi");
-                context.Response.ContentType = "text/plain";
-
-                if (context.Request.Cookies.TryGetValue("atoken", out string tokenValue) &&
-                    tokenValue.Equals(Config.AdminToken))
-                    await context.Response.WriteAsync(MemoryCache.Default.Aggregate(string.Empty,
-                        (current, item) =>
-                            current +
-                            $"{item.Key.ToUpper()}:" +
-                            $"{(item.Value as List<DnsRecordBase> ?? new List<DnsRecordBase> { new TxtRecord(DomainName.Parse("PASS"), 600, item.Value.ToString()) }).FirstOrDefault()}" +
-                            Environment.NewLine));
-                else await context.Response.WriteAsync("Token Required");
+                if (!CheckAdminToken(context)) return;
+                await context.Response.WriteAsync(MemoryCache.Default.Aggregate(string.Empty,
+                    (current, item) =>
+                        current +
+                        $"{item.Key.ToUpper()}:" +
+                        $"{(item.Value as List<DnsRecordBase> ?? new List<DnsRecordBase> {new TxtRecord(DomainName.Parse("PASS"), 600, item.Value.ToString())}).FirstOrDefault()}" +
+                        Environment.NewLine));
             });
             endpoints.Map(Config.AdminPerfix + "/cache/keys", async context =>
             {
-                context.Response.Headers.Add("X-Powered-By", "CheeDBSE/ONE");
-                context.Response.ContentType = "text/plain";
-                if (context.Request.Cookies.TryGetValue("atoken", out string tokenValue) &&
-                    tokenValue.Equals(Config.AdminToken))
-                    await context.Response.WriteAsync(string.Join(Environment.NewLine,
-                        MemoryCache.Default.Select(item => $"{item.Key}:{item.Value}").ToList()));
-                else await context.Response.WriteAsync("Token Required");
+                if (!CheckAdminToken(context)) return;
+                await context.Response.WriteAsync(string.Join(Environment.NewLine,
+                    MemoryCache.Default.Select(item => $"{item.Key}:{item.Value}").ToList()));
             });
             endpoints.Map(Config.AdminPerfix + "/cnlist/ls", async context =>
             {
-                context.Response.Headers.Add("X-Powered-By", "ArashiDNSP/ONE.Aoi");
-                context.Response.ContentType = "text/plain";
-
-                if (context.Request.Cookies.TryGetValue("atoken", out var tokenValue) &&
-                    tokenValue.Equals(Config.AdminToken))
-                    await context.Response.WriteAsync(string.Join(Environment.NewLine, DNSChina.ChinaList));
-                else await context.Response.WriteAsync("Token Required");
+                if (!CheckAdminToken(context)) return;
+                await context.Response.WriteAsync(string.Join(Environment.NewLine, DNSChina.ChinaList));
             });
             endpoints.Map(Config.AdminPerfix + "/cache/rm", async context =>
             {
-                context.Response.Headers.Add("X-Powered-By", "ArashiDNSP/ONE.Aoi");
-                context.Response.ContentType = "text/plain";
-
-                if (context.Request.Cookies.TryGetValue("atoken", out var tokenValue) &&
-                    tokenValue.Equals(Config.AdminToken))
-                {
-                    MemoryCache.Default.Trim(100);
-                    await context.Response.WriteAsync("Trim OK");
-                }
-                else await context.Response.WriteAsync("Token Required");
+                if (!CheckAdminToken(context)) return;
+                MemoryCache.Default.Trim(100);
+                await context.Response.WriteAsync("Trim OK");
             });
             endpoints.Map(Config.AdminPerfix + "/set-token", async context =>
             {
@@ -83,6 +62,17 @@ namespace Arashi.Aoi.Routes
                 }
                 else await context.Response.WriteAsync("Token Required");
             });
+        }
+
+        public static bool CheckAdminToken(HttpContext context)
+        {
+            context.Response.Headers.Add("X-Powered-By", "ArashiDNSP/ONE.Aoi");
+            context.Response.ContentType = "text/plain";
+            if (context.Request.Cookies.TryGetValue("atoken", out var tokenValue) &&
+                tokenValue.Equals(Config.AdminToken) && Config.UseAdminRoute) return true;
+            context.Response.ContentType = "text/plain";
+            context.Response.WriteAsync("Token Required");
+            return false;
         }
     }
 }
