2e3b88a9d1fce8dd8fe141edc0a1a4dcb02cd174
Milkey Tan <819774989@qq.com>
2020-12-02 10:55:29 +0800

    sth

--- Arashi.Aoi/build.cmd
+++ Arashi.Aoi/build.cmd
@@ -1,5 +1,5 @@
-dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true
-dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true
+dotnet publish -r linux-x64 -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -p:PublishReadyToRun=true
+dotnet publish -r win-x64 -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -p:PublishReadyToRun=true
 dotnet publish -r win-x86 -c Release
 dotnet publish -r win-x64 -c Release
 dotnet publish -r linux-x64 -c Release
