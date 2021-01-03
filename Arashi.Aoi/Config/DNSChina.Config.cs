1b00227cf7de350d829b9fbbd205ac22d540cd1e
Milkey Tan <819774989@qq.com>
2021-01-03 20:37:33 +0800

    fix

--- Arashi.Aoi/Config/DNSChina.Config.cs
+++ Arashi.Aoi/Config/DNSChina.Config.cs
@@ -7,7 +7,7 @@ namespace Arashi.Kestrel
         public static DNSChinaConfig Config = new DNSChinaConfig();
         public string HttpDnsEcsUrl = "http://119.29.29.29/d?dn={0}&ttl=1&ip={1}";
         public string HttpDnsUrl = "http://119.29.29.29/d?dn={0}&ttl=1";
-        public string ChinaListPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "china_whitelist.list";
+        public string ChinaListPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "China_WhiteList.List";
         public string ChinaDnsIp = "119.29.29.29";
         public bool UseHttpDns = true;
     }
