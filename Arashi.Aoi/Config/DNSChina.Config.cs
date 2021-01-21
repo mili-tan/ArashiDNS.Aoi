1bc67c8edd798dac64bcaaf0425bd4d61eb5299d
Milkey Tan <819774989@qq.com>
2021-01-21 20:49:54 +0800

    HTTPDNS

--- Arashi.Aoi/Config/DNSChina.Config.cs
+++ Arashi.Aoi/Config/DNSChina.Config.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Net.Sockets;
 
 namespace Arashi.Kestrel
 {
     public class DNSChinaConfig
     {
         public static DNSChinaConfig Config = new DNSChinaConfig();
-        public string HttpDnsEcsUrl = "http://119.29.29.29/d?dn={0}&ttl=1&ip={1}";
-        public string HttpDnsUrl = "http://119.29.29.29/d?dn={0}&ttl=1";
         public string ChinaListPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "China_WhiteList.List";
         public string ChinaDnsIp = "119.29.29.29";
         public bool UseHttpDns = true;
+
+        public string HttpDnsEcsUrl = Socket.OSSupportsIPv4
+            ? "http://119.29.29.29/d?dn={0}&ttl=1&ip={1}"
+            : "https://dopx.netlify.app/d?dn={0}&ttl=1&ip={1}";
+
+        public string HttpDnsUrl = Socket.OSSupportsIPv4
+            ? "http://119.29.29.29/d?dn={0}&ttl=1"
+            : "http://dopx.netlify.app/d?dn={0}&ttl=1";
     }
 }
