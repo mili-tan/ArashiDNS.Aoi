fc18d21497ec9df400bbc1df3d38ae431cdd72e5
Milkey Tan <819774989@qq.com>
2022-08-09 21:41:35 +0800

    update

--- Arashi.Aoi/Routes/ResolveRoutes.cs
+++ Arashi.Aoi/Routes/ResolveRoutes.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using static Arashi.AoiConfig;
 using static Arashi.Aoi.Routes.DnsQueryRoutes;
@@ -11,25 +13,32 @@ namespace Arashi.Aoi
         {
             endpoints.Map("/resolve", async context =>
             {
+                var idEnable = Config.TransIdEnable;
+                var userAgent = context.Request.Headers.UserAgent.ToString().ToLower();
                 var queryDictionary = context.Request.Query;
+                var uaList = new List<string> { "intra", "chrome", "curl" };
+
+                if (string.IsNullOrWhiteSpace(userAgent) || uaList.Any(item => userAgent.Contains(item)))
+                    idEnable = false;
+
                 if (context.Request.Method == "POST")
                 {
                     var dnsq = await DNSParser.FromPostByteAsync(context);
                     await ReturnContext(context, true,
                         DnsQuery(dnsq, context),
-                        transIdEnable: true, id: dnsq.TransactionID);
+                        transIdEnable: idEnable, id: dnsq.TransactionID);
                 }
                 else if (queryDictionary.ContainsKey("dns"))
                 {
                     var dnsq = DNSParser.FromWebBase64(context);
                     await ReturnContext(context, true,
                         DnsQuery(dnsq, context),
-                        transIdEnable: true, id: dnsq.TransactionID);
+                        transIdEnable: idEnable, id: dnsq.TransactionID);
                 }
                 else if (queryDictionary.ContainsKey("name"))
                     await ReturnContext(context, false,
                         DnsQuery(DNSParser.FromDnsJson(context, EcsDefaultMask: Config.EcsDefaultMask), context),
-                        transIdEnable: true);
+                        transIdEnable: idEnable);
                 else
                     await context.WriteResponseAsync(Startup.IndexStr, type: "text/html");
             });
