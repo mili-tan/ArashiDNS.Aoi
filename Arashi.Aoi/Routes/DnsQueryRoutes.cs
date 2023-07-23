c7e257ffa42402543ebf455545a186cdc7c5eae1
Milkey Tan <819774989@qq.com>
2023-07-24 02:59:47 +0800

    cache.arashi-msg

--- Arashi.Aoi/Routes/DnsQueryRoutes.cs
+++ Arashi.Aoi/Routes/DnsQueryRoutes.cs
@@ -220,7 +220,8 @@ namespace Arashi.Aoi.Routes
 
         public static void WriteLogAndCache(DnsMessage dnsMessage, HttpContext context = null)
         {
-            if (Config.CacheEnable)
+            if (Config.CacheEnable &&
+                !dnsMessage.AnswerRecords.Any(i => i.Name.IsEqualOrSubDomainOf(DomainName.Parse("cache.arashi-msg"))))
                 Task.Run(() =>
                 {
                     if (context != null && Config.GeoCacheEnable) DnsCache.Add(dnsMessage, context);
