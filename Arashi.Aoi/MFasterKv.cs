403c87c5f4afb4aa5b3594d11a9012fc0eabe252
Milkey Tan <819774989@qq.com>
2023-09-21 21:19:50 +0800

    MFaster

---
+++ Arashi.Aoi/MFasterKv.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Jering.KeyValueStore;
+
+namespace ArashiDNS.Tools
+{
+    class MFaster
+    {
+        public static MixedStorageKVStore<string,string> FasterKv;
+
+        public static void Init()
+        {
+            try
+            {
+                if (Directory.Exists("faster.db")) Directory.Delete("faster.db", true);
+                FasterKv = new MixedStorageKVStore<string, string>(new MixedStorageKVStoreOptions()
+                    {LogDirectory = "faster.db"});
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
