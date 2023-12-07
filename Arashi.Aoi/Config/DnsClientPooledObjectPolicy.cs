e2f740922971900e3c0614865a1bf0c90fa9f244
Milkey Tan <me@milione.cc>
2023-12-07 20:15:10 +0800

    Port

--- Arashi.Aoi/Config/DnsClientPooledObjectPolicy.cs
+++ Arashi.Aoi/Config/DnsClientPooledObjectPolicy.cs
@@ -4,10 +4,13 @@ using ARSoft.Tools.Net.Dns;
 
 namespace Arashi.Aoi
 {
-    public class DnsClientPooledObjectPolicy(IPAddress ip,int timeout) : IPooledObjectPolicy<DnsClient>
+    public class DnsClientPooledObjectPolicy(IPAddress ip,int timeout,int port) : IPooledObjectPolicy<DnsClient>
     {
 
-        public DnsClient Create() => new(ip, timeout);
+        public DnsClient Create() => new(new[] {ip},
+            new IClientTransport[]
+                {new UdpClientTransport(port == 0 ? 53 : port), new TcpClientTransport(port == 0 ? 53 : port)},
+            false, timeout);
 
         public bool Return(DnsClient obj) => true;
     }
