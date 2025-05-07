6bc95991240eb4b964fae8c05eda658bd0177288
Milkey Tan <24996957+mili-tan@users.noreply.github.com>
2025-05-07 11:17:49 +0800

    +Content-Length

--- Arashi.Aoi/MContext.cs
+++ Arashi.Aoi/MContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
 namespace Arashi
@@ -8,23 +9,38 @@ namespace Arashi
         public static async Task WriteResponseAsync(this HttpContext context, string str,
             int status = StatusCodes.Status200OK, string type = "text/plain", HeaderDictionary headers = null)
         {
-            if (headers != null)
-                foreach (var (k, v) in headers)
-                    context.Response.Headers.Add(k, v);
-            if (!context.Response.Headers.Keys.Contains("X-Powered-By"))
-                context.Response.Headers.Add("X-Powered-By", "ArashiDNSP/Aoi");
-            context.Response.ContentType = type;
-            context.Response.StatusCode = status;
+            try
+            {
+                if (headers != null)
+                    foreach (var (k, v) in headers)
+                        context.Response.Headers.Add(k, v);
+                if (!context.Response.Headers.Keys.Contains("X-Powered-By"))
+                    context.Response.Headers.Add("X-Powered-By", "ArashiDNSP/Aoi");
+                context.Response.ContentType = type;
+                context.Response.StatusCode = status;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
             await context.Response.WriteAsync(str);
         }
 
         public static async Task WriteResponseAsync(this HttpContext context, byte[] bytes,
             int status = StatusCodes.Status200OK, string type = "text/plain")
         {
-            if (!context.Response.Headers.Keys.Contains("X-Powered-By"))
-                context.Response.Headers.Add("X-Powered-By", "ArashiDNSP/Aoi");
-            context.Response.ContentType = type;
-            context.Response.StatusCode = status;
+            try
+            {
+                if (!context.Response.Headers.Keys.Contains("X-Powered-By"))
+                    context.Response.Headers.Add("X-Powered-By", "ArashiDNSP/Aoi");
+                context.Response.ContentType = type;
+                context.Response.StatusCode = status;
+                context.Response.Headers["Content-Length"] = bytes.Length.ToString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
             await context.Response.Body.WriteAsync(bytes);
         }
     }
